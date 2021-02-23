8f17a9bedf83b5e39a8479f5b17726b0ba6b5078
Enrique Flores <ehf.81@hotmail.com>
2021-02-23 00:58:30 -0800

    edit

--- js/ex2.js
+++ js/ex2.js
@@ -1,4 +1,15 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 //use filter to get odd numbers
 const oddnum = numbers.filter (number => number%2);
+//use filter to get numbers divisible by 2 or 5
+const num25 = numbers.filter( x % 2 == 0 || x % 5 == 0);
+//use filter to get numbers divisible by 3 squared
+
+//use reduce to get the sum of numbers divisible by 5 squared
+
+
+
+
 console.log(oddnum)
+console.log(num25)
