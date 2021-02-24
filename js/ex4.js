65e5fce1505b617e2637e1927a4e0439eb0caf79
Enrique Flores <ehf.81@hotmail.com>
2021-02-23 17:56:45 -0800

    edit

--- js/ex4.js
+++
@@ -1,4 +0,0 @@
-anArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-//console.log(anArray.filter(value => value % 2 === 0).map(x => x * 2));
-console.log(anArray.map(x => x * 2).filter(value => value % 2 === 0));
